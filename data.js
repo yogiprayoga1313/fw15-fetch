8b29a8e76ac9e73d47f713ff9050a5754a83d5f0
Yogi Prayoga <yogiprayoga1333@gmail.com>
2023-03-29 16:08:38 +0700

    Update Tugas Week 4, No 6 'sudah di deploy'

--- data.js
+++ data.js
@@ -1,9 +1,9 @@
 const endPoint = "https://jsonplaceholder.typicode.com/users/"
 // console.log(fetch(endPoint))
 fetch(endPoint)
-  .then((result) => result.json())
-  .then((data) =>{
-    let dataTable = `<thead>
+    .then((result) => result.json())
+    .then((data) => {
+        let dataTable = `<thead class="bg-blue-400 h-[50px] font-semibold text-[20px] ">
     <tr id="tableList">
     <th class="id">ID</th>
     <th class="name">Name</th>
@@ -14,25 +14,24 @@ fetch(endPoint)
     <th class="website">Website</th>
     <th class="company">Company</th>
 </tr></thead>
-<tbody>`
+<tbody class="gap-3 bg-blue-200 gap-12 self-center">`
 
-    data.forEach(element => {
-    // console.log(element)
-    dataTable+=`<tr>
-    <td>${element.id}</td>
+        data.forEach(element => {
+            // console.log(element)
+            dataTable += `<tr>
+    <td class="font-semibold pl-[15px]">${element.id}</td>
     <td>${element.name}</td>
     <td>${element.username}</td>
     <td>${element.email}</td>
     <td>${element.address.street},${element.address.city}</td>
     <td>${element.phone}</td>
     <td>${element.website}</td>
-    <td>${element.company.name}</td>
+    <td class="font-semibold">${element.company.name}</td>
 </tr>` ;
 
-   });
-   dataTable+='</tbody>'
-console.log(dataTable)
-   document.getElementById('user').innerHTML=dataTable
-   
-})
-  
+        });
+        dataTable += '</tbody>'
+        console.log(dataTable)
+        document.getElementById('user').innerHTML = dataTable
+
+    })
